26a19875b152c5ec77ff959dcbc6e81bb9ff24b5
Rolf Geuenich <rolf.geuenich@priomni.de>
2023-09-15 12:45:14 +0200

    Initial commit for this tool

---
+++ README.md
@@ -0,0 +1,57 @@
+# TTS-Radio-Generator
+
+I wanted a tool to generate sound files as radio calls for my DCS missions.
+It should use a Text-To-Speech engine, add some white noise and a click sound at the start and the end of the "transmission".
+I searched the web and found some existing projects which sounds more or less the tool I want to have.
+
+- [radio-message-generator][1]:
+    No binary release and I don't want to build it.
+
+- [DCS_RadioComm_VoiceLineGenerator][2]:
+    A nearly perfect match, but I needed the features which are planned for the future.
+
+I had the idea to fork DCS_RadioComm_VoiceLineGenerator, add the featrures I need by myself and make a merge request.
+But the way DCS_RadioComm_VoiceLineGenerator is coded is above my Python skill. So I was forced to create this tool from scratch.
+But the code adamclmns was a great help and good insperation. So many thanks to him for his work!
+
+# Features
+
+- Use Windows or Google Text-To-Speach engine to generate sound files
+- Choose the voice used for generation
+- Use a csv text file for batch processing, each line creates one sond file
+- Optionally add white noise to the sound files
+- Optionally add start and end click sounds
+- target codec is ogg to save disk space and use it for DCS
+- Configurable sound volumes for white noise and target file
+- Configurable values for Low and High pass filters
+
+# Prerequisits
+
+If you want to run the script with Python you need:
+
+- Python
+- All libraries from requirements.txt (run install.cmd to get them)
+- ffmpeg installed by chocolaty
+
+I'll will to to create a all-in-one exe file for a people who don't want to setup Python.
+
+# How to use
+
+- Save this repo somewhere onto your hard disk
+- Install all prerequisits
+- Run `tts-radio-generator.py` without any parameters
+- It will use `./conf/default.yml` and `./conf/example.csv` to create sound files with Microsoft TTS
+- Install as much languages as you like to your windows, if you want to use Windows Text-To-Speech with another voice
+- Copy `default.yml` to a new file, e.g. `custom-myproject.yml`
+- Copy `example.csv` to a new file, e.g. `custom-myproject.csv`
+- Change both files to match your needs
+- Run `tts-radio-generator.py -c ./conf/custom-myproject.yml`
+- If you want to use Google TTS you can use `default-google.yml` as a template
+
+# ToDo
+
+- Create a EXE all-in-one distribution
+- Maybe add more TTS engines like AWS or Azure
+
+[1]: <https://github.com/akaAgar/radio-message-generator>
+[2]: <https://github.com/adamclmns/DCS_RadioComm_VoiceLineGenerator>
---
+++ tts-radio-generator.py
@@ -0,0 +1,204 @@
+import os
+import pyttsx3
+import sys
+import getopt
+from google.cloud import texttospeech
+from pydub import AudioSegment, generators
+from ruamel.yaml import YAML
+
+###############################################################################
+# Function to show installed tts voices
+###############################################################################
+def showVoices():
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    print("\nCurrently available System voices:")
+    i=0
+    configuredVoiceFound = False
+    for voice in voices:
+        if voice.id == conf_voice:
+            configuredVoiceFound = True
+        print( f"[{i}]: {voice.id}" )
+        i=i+1
+    print("You can use the id of the desired voice in the config file.")
+    if not configuredVoiceFound:
+        print( f"\nERROR: Configured voice not found on your system!" )
+        print( f"Config: {conf_voice}")
+        print("Please double check your configuration for typos.")
+        print("System default [0] is used!")
+    else:
+        print( f"Using {conf_voice}")
+    print("\n")
+
+###############################################################################
+# Function to find silence at the start or end of a sound
+###############################################################################
+def detect_leading_silence(sound, silence_threshold=-50.0, chunk_size=10):
+    # sound is a pydub.AudioSegment
+    # silence_threshold in dB
+    # chunk_size in ms
+    # iterate over chunks until you find the first one with sound
+    trim_ms = 0
+
+    assert chunk_size > 0 # to avoid infinite loop
+    while sound[trim_ms:trim_ms+chunk_size].dBFS < silence_threshold and trim_ms < len(sound):
+        trim_ms += chunk_size
+
+    return trim_ms
+
+###############################################################################
+# Run when script is executed.
+###############################################################################
+if __name__ == '__main__':
+    print("\nScript started\n")
+
+    confFilePath = "./conf/default.yml"
+
+    # Parse command line arguments
+    argv = sys.argv[1:]
+    try:
+        options, args = getopt.getopt(argv, "c:")
+    except:
+        print("Error: Command line argument unknown")
+
+    for name, value in options:
+        if name in ['-c']:
+            confFilePath = value
+
+    # Read the configuration file
+    print( f"\nUsing configuration: {confFilePath}" )
+    confFile = open(confFilePath, "r")
+
+    yaml=YAML(typ='safe')
+    conf=yaml.load(confFile)
+
+    # Configuration values
+    outFolderWav = conf['Config']['Output']['DirWav']
+    outFolderOgg = conf['Config']['Output']['DirOgg']
+
+    conf_voice = conf['Config']['TTS']['Voice']
+    conf_volume = conf['Config']['Audio']['Volume']
+    conf_filter_boost = conf['Config']['Audio']['Filter']['Boost']
+    conf_white_noise_decrease = conf['Config']['Audio']['WhiteNoise']['VolumeDecrease']
+    conf_high_pass_filter_arg = conf['Config']['Audio']['Filter']['ArgHighPass']
+    conf_low_pass_filter_arg = conf['Config']['Audio']['Filter']['ArgLowPass']
+    # Audio files for 'click in' and 'click out'
+    conf_IN_WAV = conf['Config']['Audio']['PreSound']['File']
+    conf_OUT_WAV = conf['Config']['Audio']['PostSound']['File']
+    conf_ffmpegHome = conf['Config']['FfMpegHome']
+    conf_TtsEngine = conf['Config']['TTS']['Engine']
+
+    print("Using FFMPEG_HOME: ", conf_ffmpegHome)
+
+    # Show all installed voices and the one which will be used
+    if conf_TtsEngine == 'Windows':
+        print("Using TextToSpeech engine from Windows.")
+        showVoices()
+        engine = pyttsx3.init()
+        engine.setProperty('volume', 1.0)
+        engine.setProperty('rate', 200)
+        engine.setProperty('voice', conf_voice)
+
+    if conf_TtsEngine == 'Google':
+        print("Using TextToSpeech engine from Google.")
+        print("\nHere you will find a list with all voices:")
+        print("https://cloud.google.com/text-to-speech/docs/voices\n")
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = conf['Config']['TTS']['CredFile']
+        client = texttospeech.TextToSpeechClient()
+        voice = texttospeech.VoiceSelectionParams(
+            language_code=(conf['Config']['TTS']['Voice'])[:5],
+            name=conf['Config']['TTS']['Voice']
+        )
+        audio_config = texttospeech.AudioConfig(
+            audio_encoding=texttospeech.AudioEncoding.LINEAR16
+        )
+
+    # Read file with text lines for tts
+    print( "Reading text lines from csv file\n" )
+    textfile = open( conf['Config']['InputCsv'], 'r' )
+    allLines = textfile.readlines()
+    # Remove all blank lines
+    allLines = (line.rstrip() for line in allLines)
+    allLines = (line for line in allLines if line)
+
+    # Process each line with text
+    for line in allLines:
+
+        fileNameWav = line.replace(" ", "-") + ".wav"
+        outFullPathWav = outFolderWav + "/" + fileNameWav
+
+        # Run TextToSpeech and save the result as WAV file
+        if conf_TtsEngine == 'Windows':
+            engine.save_to_file(line, outFullPathWav)
+            engine.runAndWait()
+            print(f'Temporary wav file: {outFullPathWav}')
+
+        if conf_TtsEngine == 'Google':
+            synthesis_input = texttospeech.SynthesisInput(text=line)
+            response = client.synthesize_speech(
+                input=synthesis_input, voice=voice, audio_config=audio_config
+            )
+            # The response's audio_content is binary.
+            with open(outFullPathWav, "wb") as out:
+                # Write the response to the output file.
+                out.write(response.audio_content)
+                print(f'Temporary wav file: {outFullPathWav}')
+
+        # Audio manipulation
+        audioStream = AudioSegment.from_file(outFullPathWav)
+
+        if conf['Config']['Audio']['Trimm']:
+            audioDuration=len(audioStream)
+            start_trim = detect_leading_silence(audioStream)
+            end_trim = detect_leading_silence(audioStream.reverse())
+
+            print( f"audioDuration: {audioDuration}" )
+            print( f"start_trim: {start_trim}" )
+            print( f"end_trim: {end_trim}" )
+
+            audioStreamTrimmed = audioStream[start_trim:audioDuration-end_trim]
+            audioStream = audioStreamTrimmed
+            audioDuration=len(audioStream)
+            print( f"New audioDuration: {audioDuration}" )
+
+        if conf['Config']['Audio']['Filter']['Apply']:
+            audioStream = audioStream.high_pass_filter(conf_high_pass_filter_arg)
+            audioStream = audioStream.low_pass_filter(conf_low_pass_filter_arg)
+            audioStream = audioStream + conf_filter_boost
+
+        # Adding pre sound
+        if conf['Config']['Audio']['PreSound']['Add']:
+            print( f"Adding pre sound file:  '{conf_IN_WAV}'" )
+            startClick = AudioSegment.from_wav(conf_IN_WAV)
+            audioStream = startClick + audioStream
+
+        # Adding post sound
+        if conf['Config']['Audio']['PostSound']['Add']:
+            print( f"Adding post sound file: '{conf_OUT_WAV}'\n" )
+            endClick = AudioSegment.from_wav(conf_OUT_WAV)
+            audioStream = audioStream + endClick
+
+        audioDuration=len(audioStream)
+
+        # white noise profile generated
+        if conf['Config']['Audio']['WhiteNoise']['Add']:
+            print( f"Adding white noise" )
+            whiteNoise = generators.WhiteNoise().to_audio_segment(duration=audioDuration)
+            audioStream = audioStream.overlay(whiteNoise - conf_white_noise_decrease)
+
+        # bump volume of all audio
+        audioStream = audioStream + conf_volume
+
+        # Save target file
+        fileNameOgg = fileNameWav.replace(".wav", "")
+        postfix = ""
+        if conf['Config']['Output']['AddLengthPostfix']:
+            postfix = f"_{audioDuration}"
+            postfix = postfix[:-1]
+            print( f"Soundfile is {audioDuration} ms long. Adding postfix {postfix}")
+        fileNameOgg = f"{fileNameOgg}{postfix}.ogg"
+        outFullPathOgg = outFolderOgg + "/" + fileNameOgg
+        print( f"Saving target file to: {outFullPathOgg}\n" )
+        audioStream.export(outFullPathOgg, format='ogg')
+
+    print("\nSkript finished!\n")
